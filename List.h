7ff9e1dac3e3b7beaaf4bdcb1fb0491e9b81d46a
Anthony Bloch <ajbloch@stcloudstate.edu>
2017-12-02 15:02:55 -0600

    commit

--- List.h
+++ List.h
@@ -14,6 +14,8 @@ namespace csci331
 		{
 			int value_i;
 			string value_s;
+			Key* ancestor;
+			Key* child;
 			Key* prev_sibling;
 			Key* next_sibling;
 		};
@@ -23,42 +25,83 @@ namespace csci331
 
 		List(int i_or_s) { int_or_string = i_or_s; first = NULL; last = NULL; }
 
-		Key* get_key(int entry_i, string entry_s, Key* prev_link, Key* next_link)
+		Key* get_key(int entry_i, string entry_s, Key* ancestor_link, Key* child_link, Key* prev_link, Key* next_link)
 		{
 			Key *temp;
 			temp = new Key;
 			temp->value_i = entry_i;
 			temp->value_s = entry_s;
+			temp->ancestor = ancestor_link;
+			temp->child = child_link;
 			temp->prev_sibling = prev_link;
 			temp->next_sibling = next_link;
 			return temp;
 		}
 
-		void insert(int entry_i, string entry_s)
+		void insert(int entry_i, string entry_s, Key* ancestor_link, Key* child_link)
 		{
 			Key* prev;
 			if (first == NULL&&last == NULL)
 			{ // this is the first key being inserted in the list
-				first = get_key(entry_i, entry_s, NULL, NULL);
+				first = get_key(entry_i, entry_s, ancestor_link, child_link, NULL, NULL);
 				last = first;
 			}
-			else if (int_or_string>0 && (entry_i < first->value_i || entry_i == first->value_i&&entry_s<first->value_s) || int_or_string<1 && (entry_s < first->value_s || entry_s == first->value_s&&entry_i<first->value_i))
+			else if (int_or_string>0 && (entry_i < first->value_i || entry_i == first->value_i&&entry_s <= first->value_s) || int_or_string<1 && (entry_s < first->value_s || entry_s == first->value_s&&entry_i <= first->value_i))
 			{ // the key is the first in the list
-				first->prev_sibling = get_key(entry_i, entry_s, NULL, first);
+				first->prev_sibling = get_key(entry_i, entry_s, ancestor_link, child_link, NULL, first);
 				first = first->prev_sibling;
 			}
-			else if (int_or_string>0 && (entry_i > last->value_i || entry_i == last->value_i&&entry_s>last->value_s) || int_or_string<1 && (entry_s > last->value_s || entry_s == last->value_s&&entry_i>last->value_i))
+			else if (int_or_string>0 && (entry_i > last->value_i || entry_i == last->value_i&&entry_s >= last->value_s) || int_or_string<1 && (entry_s > last->value_s || entry_s == last->value_s&&entry_i >= last->value_i))
 			{ // the key is the last in the list
-				last->next_sibling = get_key(entry_i, entry_s, last, NULL);
+				last->next_sibling = get_key(entry_i, entry_s, ancestor_link, child_link, last, NULL);
 				last = last->next_sibling;
 			}
 			else
 			{
 				prev = first;
-				while (int_or_string>0 && (entry_i > prev->value_i || entry_i == prev->value_i&&entry_s>prev->value_s) || int_or_string<1 && (entry_s > prev->value_s || entry_s == prev->value_s&&entry_i>prev->value_i))
+				while (int_or_string>0 && (entry_i > prev->value_i || entry_i == prev->value_i&&entry_s >= prev->value_s) || int_or_string<1 && (entry_s > prev->value_s || entry_s == prev->value_s&&entry_i >= prev->value_i))
 					prev = prev->next_sibling;
-				prev->prev_sibling->next_sibling = get_key(entry_i, entry_s, prev->prev_sibling, prev->prev_sibling->next_sibling);
+				prev->prev_sibling->next_sibling = get_key(entry_i, entry_s, ancestor_link, child_link, prev->prev_sibling, prev->prev_sibling->next_sibling);
 				prev->prev_sibling = prev->prev_sibling->next_sibling;
+			}
+		}
+
+		Key* search(int entry_i, string entry_s)
+		{
+			Key* prev;
+			prev = first;
+			while (prev != NULL && (int_or_string>0 && (entry_i > prev->value_i || entry_i == prev->value_i&&entry_s > prev->value_s) || int_or_string<1 && (entry_s > prev->value_s || entry_s == prev->value_s&&entry_i > prev->value_i)))
+				prev = prev->next_sibling;
+			if (prev != NULL && (int_or_string > 0 && (entry_i != prev->value_i || entry_s != prev->value_s) || int_or_string < 1 && (entry_s != prev->value_s || entry_i != prev->value_i)))
+				prev = NULL;
+			return prev;
+		}
+
+		void external_search(int entry_i, string entry_s)
+		{
+			Key* temp = search(entry_i, entry_s);
+			if (temp == NULL)
+				cout << "That key is NOT in the list.\n";
+			else
+				cout << "That key is in the list.\n";
+		}
+
+		void remove(int entry_i, string entry_s)
+		{
+			Key* temp = search(entry_i, entry_s);
+			if (temp == NULL)
+				cout << "That key is NOT in the list.\n";
+			else
+			{
+				if (temp->prev_sibling != NULL)
+					temp->prev_sibling->next_sibling = temp->next_sibling;
+				else
+					first = temp->next_sibling;
+				if (temp->next_sibling != NULL)
+					temp->next_sibling->prev_sibling = temp->prev_sibling;
+				else
+					last = temp->prev_sibling;
+				delete temp;
 			}
 		}
 
@@ -87,9 +130,28 @@ namespace csci331
 				out_s << cursor->value_i << ' ' << cursor->value_s << ' ';
 				cursor = cursor->prev_sibling;
 			}
+			cout << endl;
 		}
 
+		int low_key_int()
+		{
+			return first->value_i;
+		}
 
+		string low_key_string()
+		{
+			return first->value_s;
+		}
+
+		int high_key_int()
+		{
+			return last->value_i;
+		}
+
+		string high_key_string()
+		{
+			return last->value_s;
+		}
 	};
 }
 #endif
--- Main.cpp
+++ Main.cpp
@@ -10,23 +10,50 @@ int main()
 	int entry_i;
 	string entry_s;
 	int int_or_string;
+	char ch;
+	bool flag = true;
 	cout << "Would you like to sort by integers (enter number > 0) or by strings (enter number <1)?";
 	cin >> int_or_string;
 	List l(int_or_string);
-	char ch = 'y';
-	while (ch == 'y' || ch == 'Y')
+	while (flag)
 	{
-		cout << "Please enter an integer to insert:";
-		cin >> entry_i;
-		cout << "Please enter a string to insert:";
-		cin >> entry_s;
-		l.insert(entry_i, entry_s);
-		cout << "Would you like to continue (y/n)?";
+		cout << "Please enter a command:";
 		cin >> ch;
-	}
-	cout << "The length of the list is " << l.length() << endl;
-	l.print(cout);
-	cin >> ch;
+		if (ch == 'i') // insert
+		{
+			cout << "Please enter an integer to insert:";
+			cin >> entry_i;
+			cout << "Please enter a string to insert:";
+			cin >> entry_s;
+			l.insert(entry_i, entry_s, NULL, NULL);
+		}
+		else if (ch == 'r') // remove
+		{
+			cout << "Please enter an integer to remove:";
+			cin >> entry_i;
+			cout << "Please enter a string to remove:";
+			cin >> entry_s;
+			l.remove(entry_i, entry_s);
+		}
+		else if (ch == 'l') // length
+			cout << "The length of the list is " << l.length() << endl;
+		else if (ch == 'p') // print
+			l.print(cout);
+		else if (ch == 's') //search
+		{
+			cout << "Please enter an integer to search:";
+			cin >> entry_i;
+			cout << "Please enter a string to search:";
+			cin >> entry_s;
+			l.external_search(entry_i, entry_s);
+		}
+		else if (ch == 'b') // bottom
+			cout << l.low_key_int() << ' ' << l.low_key_string() << endl;
+		else if (ch == 't') // top
+			cout << l.high_key_int() << ' ' << l.high_key_string() << endl;
+		else
+			flag = false;
 
+	}
 	return 0;
 }

0350e74b064ae0277a02b0387349a9783b5792cd
Anthony Bloch <ajbloch@stcloudstate.edu>
2017-12-02 13:31:06 -0600

    commit

--- List.h
+++ List.h
@@ -1,95 +1,32 @@
-#ifndef LIST_H
-#define LIST_H
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include "List.h"
 using namespace std;
+using namespace csci331;
 
-namespace csci331
+int main()
 {
-	class List
+	int entry_i;
+	string entry_s;
+	int int_or_string;
+	cout << "Would you like to sort by integers (enter number > 0) or by strings (enter number <1)?";
+	cin >> int_or_string;
+	List l(int_or_string);
+	char ch = 'y';
+	while (ch == 'y' || ch == 'Y')
 	{
-	public:
-		struct Key
-		{
-			int value_i;
-			string value_s;
-			Key* prev_sibling;
-			Key* next_sibling;
-		};
+		cout << "Please enter an integer to insert:";
+		cin >> entry_i;
+		cout << "Please enter a string to insert:";
+		cin >> entry_s;
+		l.insert(entry_i, entry_s);
+		cout << "Would you like to continue (y/n)?";
+		cin >> ch;
+	}
+	cout << "The length of the list is " << l.length() << endl;
+	l.print(cout);
+	cin >> ch;
 
-		Key* first;
-		Key* last;
-
-		List() { first = NULL; last = NULL; }
-
-		Key* get_key(int entry_i, string entry_s, Key* prev_link, Key* next_link)
-		{
-			Key *temp;
-			temp = new Key;
-			temp->value_i = entry_i;
-			temp->value_s = entry_s;
-			temp->prev_sibling = prev_link;
-			temp->next_sibling = next_link;
-			return temp;
-		}
-
-		void insert(int entry_i, string entry_s)
-		{
-			Key* prev;
-			if (first == NULL&&last == NULL)
-			{
-				first = get_key(entry_i, entry_s, NULL, NULL);
-				last = first;
-			}
-			else if (entry_i < first->value_i)
-			{
-				first->prev_sibling = get_key(entry_i, entry_s, NULL, first);
-				first = first->prev_sibling;
-			}
-			else if (entry_i > last->value_i)
-			{
-				last->next_sibling = get_key(entry_i, entry_s, last, NULL);
-				last = last->next_sibling;
-			}
-			else
-			{
-				prev = first;
-				while (entry_i > prev->value_i)
-					prev = prev->next_sibling;
-				prev->prev_sibling->next_sibling = get_key(entry_i, entry_s, prev->prev_sibling, prev->prev_sibling->next_sibling);
-				prev->prev_sibling = prev->prev_sibling->next_sibling;
-			}
-		}
-
-		int length() const
-		{
-			Key *cursor;
-			int length = 0;
-			for (cursor = first; cursor != NULL; cursor = cursor->next_sibling)
-				++length;
-			return length;
-		}
-
-		void print(ostream& out_s)
-		{
-			Key *cursor;
-			cursor = first;
-			while (cursor != NULL)
-			{
-				out_s << cursor->value_i << ' ' << cursor->value_s << ' ';
-				cursor = cursor->next_sibling;
-			}
-			cout << endl;
-			cursor = last;
-			while (cursor != NULL)
-			{
-				out_s << cursor->value_i << ' ' << cursor->value_s << ' ';
-				cursor = cursor->prev_sibling;
-			}
-		}
-
-
-	};
+	return 0;
 }
-#endif

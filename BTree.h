f5eacf48f38ae7754042fff961cbb9706d115fb7
jpkuta <jpkuta@gmail.com>
2017-12-07 03:56:33 -0600

    Update BTree.h
    added record struct
    - blocks will hold up to 6 records 
    - blocks make up the leaves of the b+treaa

--- BTree.h
+++ BTree.h
@@ -15,27 +15,33 @@ namespace csci331
     string     in_file, out_file; //input and output file (string) names
    static int MAXBLOCKSIZE = 6; //Maximum number of records/keys that can be stored in a block
 
- struct Node //structure for keys, copied from List.h
+ struct Node //structure for keys, copied from List.h/replace or interchange with block
 		{
-			int value_i;
-			string value_s;
+			int value_i; //Key value
+			string value_s; //set to null if just key node
 			Node* ancestor;
 			Node* child;
-			Node* prev_sibling;
-			Node* next_sibling;
+			//Node* prev_sibling;
+			//Node* next_sibling;
 			Node* prev_sequence;
 			Node* next_sequence;
+			bool ROOT; // true if node is the root of the b+tree
 		};
 
-   struct Block //structure for blocks, make up leaf nodes of tree, holds records in the form of a list
+   struct Block //structure for blocks, made up leaf nodes of tree, holds records in the form of a list
    {
-       bool leaf; //true if block is a leaf node
-       bool ROOT; // true if block is the root of the b+tree
-       Node* parent; //Parent node of the block
+       //bool leaf; //true if block is a leaf node
+       Node* parent; //pointer to the parent node of the block
        Block* next_sibling; // sibling block(node) to the right
        Block*  prev_sequence; // sibling block(node) to the left
+       int BlockSize;//Number of records currently stored in the block, restricted to my MAXBLOCKSIZE
 
    };
+   struct record
+   {
+       string record[];
+       int key;
+    };
 
     BTREE(int i_or_s, int Size, string InFile, string OutFile)
     {
@@ -43,7 +49,6 @@ namespace csci331
       MAX_SIZE      = Size;
       in_file       = InFile; //File of input data
       out_file      = OutFile; //Output file
-      T_Size        = 1;
     }
 
 
